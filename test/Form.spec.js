14070e4422c4d8d89fefc3f116df60d29c1f7be3
Sergei Droganov <serge@droganov.ru>
2019-09-12 12:28:35 +0300

    add files

--- test/Form.spec.js
+++ test/Form.spec.js
@@ -3,105 +3,83 @@ const makeForm = require('./makeForm');
 
 describe('live=false', () => {
   const emit = makeForm({}, 'prevalidate', 'validate', 'postvalidate');
-  it('does not not validate', () =>
-    emit('change', '#text')
-      .should
-      .eventually
-      .deep
-      .equal({}));
+  it('does not not validate', () => emit('change', '#text').should.eventually.deep.equal({}));
 });
 
 describe('live=true', () => {
   const emit = makeForm({ live: true }, 'prevalidate', 'validate', 'postvalidate');
   it('validates', () =>
-    emit('change', '#text', 'input')
-      .should
-      .eventually
-      .deep
-      .equal({
-        prevalidate: 'prevalidate',
-        validate: 'validate',
-        postvalidate: 'postvalidate',
-      }));
+    emit('change', '#text', 'input').should.eventually.deep.equal({
+      prevalidate: 'prevalidate',
+      validate: 'validate',
+      postvalidate: 'postvalidate',
+    }));
 });
 
 describe('onFormChange', () => {
   it('validates and fires onFormChange', () =>
-    makeForm({}, 'onFormChange', 'onFormComplete', 'validate')('change', '#text', 'input')
-      .should
-      .eventually
-      .deep
-      .equal({
-        validate: 'validate',
-        onFormChange: 'onFormChange',
-      }));
+    makeForm({}, 'onFormChange', 'onFormComplete', 'validate')(
+      'change',
+      '#text',
+      'input',
+    ).should.eventually.deep.equal({
+      validate: 'validate',
+      onFormChange: 'onFormChange',
+    }));
   it('ignores onFormComplete', () =>
-    makeForm({}, 'onFormComplete', 'validate')('change', '#text', 'input')
-      .should
-      .eventually
-      .deep
-      .equal({}));
+    makeForm({}, 'onFormComplete', 'validate')(
+      'change',
+      '#text',
+      'input',
+    ).should.eventually.deep.equal({}));
 });
 
 describe('onFormComplete', () => {
   it('validates and prefers onFormComplete', () =>
-    makeForm({}, 'onFormChange', 'onFormComplete', 'validate')('change', '#text')
-      .should
-      .eventually
-      .deep
-      .equal({
-        validate: 'validate',
-        onFormComplete: 'onFormComplete',
-      }));
+    makeForm({}, 'onFormChange', 'onFormComplete', 'validate')(
+      'change',
+      '#text',
+    ).should.eventually.deep.equal({
+      validate: 'validate',
+      onFormComplete: 'onFormComplete',
+    }));
   it('validates and emits onFormChange id onFormComplete is not provided', () =>
-    makeForm({}, 'onFormChange', 'validate')('change', '#text')
-      .should
-      .eventually
-      .deep
-      .equal({
-        validate: 'validate',
-        onFormChange: 'onFormChange',
-      }));
+    makeForm({}, 'onFormChange', 'validate')('change', '#text').should.eventually.deep.equal({
+      validate: 'validate',
+      onFormChange: 'onFormChange',
+    }));
 });
 
 describe('onFormSubmit', () => {
   const emit = makeForm({}, 'onFormSubmit', 'validate');
   it('validates and then triggers onFormSubmit', () =>
-    emit('submit', 'form')
-      .should
-      .eventually
-      .deep
-      .equal({
-        validate: 'validate',
-        onFormSubmit: 'onFormSubmit',
-      }));
+    emit('submit', 'form').should.eventually.deep.equal({
+      validate: 'validate',
+      onFormSubmit: 'onFormSubmit',
+    }));
 });
 
 describe('onValidationError', () => {
   const validate = () => Promise.reject([]); // eslint-disable-line
   const emit = makeForm({ validate }, 'onFormSubmit', 'onValidationError');
   it('triggers onValidationError', () =>
-    emit('submit', 'form')
-      .should
-      .eventually
-      .deep
-      .equal({
-        onValidationError: 'onValidationError',
-      }));
+    emit('submit', 'form').should.eventually.deep.equal({
+      onValidationError: 'onValidationError',
+    }));
 });
 
 describe('validation flow', () => {
   const data = {};
-  const prevalidate = (form) => {
-    const prevalidatedForm = Object.assign({}, form, { prevalidated: true });
+  const prevalidate = form => {
+    const prevalidatedForm = { ...form, prevalidated: true };
     return prevalidatedForm;
   };
-  const validate = (form) => {
-    const validatedForm = Object.assign({}, form, { validated: true });
+  const validate = form => {
+    const validatedForm = { ...form, validated: true };
     return Promise.resolve(validatedForm);
   };
-  const postvalidate = (form) => {
-    const postvalidatedForm = Object.assign(data, form, { postvalidated: true });
+  const postvalidate = form => {
+    const postvalidatedForm = { ...data, ...form, postvalidated: true };
     return postvalidatedForm;
   };
 
@@ -109,10 +87,7 @@ describe('validation flow', () => {
   it('passes data down the validation pipeline', () =>
     emit('submit', 'form')
       .then(result => Object.assign(result, data))
-      .should
-      .eventually
-      .deep
-      .equal({
+      .should.eventually.deep.equal({
         text: 'text',
         radio: 'radio',
         checkbox: 'checkbox',
--- test/makeEventEmitter.js
+++ test/makeEventEmitter.js
@@ -1,27 +1,17 @@
-export default (form, tracker) =>
-  (event, source, eventType) => {
-    const element = form.find(source);
-    // eslint-disable-next-line
-    const { name, type, defaultValue, value } = element.props();
-    element.simulate(
-      event,
-      {
-        nativeEvent: {
-          type: eventType || event,
-        },
-        preventDefault: () => null,
-        target: {
-          name,
-          value: value || defaultValue,
-          type,
-        },
-      },
-    );
-    return new Promise(
-      resolve =>
-        setTimeout(
-          () => resolve(tracker.result),
-          32,
-        ),
-    );
-  };
+export default (form, tracker) => (event, source, eventType) => {
+  const element = form.find(source);
+  // eslint-disable-next-line
+  const { name, type, defaultValue, value } = element.props();
+  element.simulate(event, {
+    nativeEvent: {
+      type: eventType || event,
+    },
+    preventDefault: () => null,
+    target: {
+      name,
+      value: value || defaultValue,
+      type,
+    },
+  });
+  return new Promise(resolve => setTimeout(() => resolve(tracker.result), 32));
+};
